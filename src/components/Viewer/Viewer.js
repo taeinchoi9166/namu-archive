6eec033b9b6ce4d6af1dcf4136b47c5b9eb29ca5
taein <taein1009@naver.com>
2019-06-20 15:32:50 +0900

    20190620 15:32

--- src/components/Viewer/Viewer.js
+++ src/components/Viewer/Viewer.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 //import pdfWindow from 'electron-pdf-window';
-//import pdf from './../../pdfiles/pdf-test.pdf';
-import {Document, Page} from 'react-pdf/dist/entry.webpack';
+import pdf from './pdf-test.pdf';
+import {Document, Page} from 'react-pdf';
 
 class Viewer extends Component{
     render(){
@@ -9,7 +9,7 @@ class Viewer extends Component{
         //console.log(pdf);
         return(
             <section className="pdf-viewer">
-                <Document file='./sample.pdf' height="100%" noData="nope." >
+                <Document file={pdf} height="100%" noData="nope." onLoadSuccess={console.log("ok")}>
                     <Page pageNumber={1}/>
                 </Document>
                 {/*<embed src={path.resolve(__dirname,"./../../../pdf-test.pdf")} type="application/pdf" width="100%" height="100%"></embed>*/}
